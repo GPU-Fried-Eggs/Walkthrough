5c4a671a6fbf08a0a522678d75d52d401ece5a7b
深海里有坏龙龙 <seele.peng@gmail.com>
2023-02-14 10:55:59 +0200

    fix(app): add noGMS case

--- app/src/main/java/com/kotlin/walkthrough/ui/location/Location.kt
+++ app/src/main/java/com/kotlin/walkthrough/ui/location/Location.kt
@@ -1,16 +1,18 @@
 package com.kotlin.walkthrough.ui.location
 
 import android.content.Context
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.common.GoogleApiAvailability
 import com.kotlin.walkthrough.R
 
 @Composable
@@ -19,25 +21,44 @@ fun Location(
     locationViewModel: LocationViewModel = LocationViewModel(context)
 ) {
     val location by locationViewModel.getLocationLiveData().observeAsState()
+    var enable: Boolean by remember { mutableStateOf(false) }
     var show: Boolean by remember { mutableStateOf(false) }
+
+    GoogleApiAvailability.getInstance().apply {
+        enable =
+            when(isGooglePlayServicesAvailable(context)) {
+                1 -> true
+                else -> false
+            }
+    }
 
     LocationRequest(context, show) {
         locationViewModel.getLocationLiveData().getLocationData()
         show = it
     }
 
-    Column {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
         Button(
             onClick = {
                 locationViewModel.getLocationLiveData().getLocationData()
                 show = true
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
         ) {
             Text(stringResource(R.string.location_button))
         }
-        Text(location?.latitude.toString())
-        Text(location?.longitude.toString())
+        Row(
+            modifier = Modifier.fillMaxWidth(0.8f).align(Alignment.CenterHorizontally),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (enable) {
+                Text(location?.latitude.toString())
+                Text(location?.longitude.toString())
+            } else {
+                Text(stringResource(R.string.location_noGMS))
+            }
+        }
     }
 }
 
